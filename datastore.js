951d43faff358880dd21e2ff1ef23cdc123da0cb
lizrary <46548997+lizrary@users.noreply.github.com>
2019-05-16 20:17:41 -0400

    Create datastore.js

---
+++ datastore.js
@@ -0,0 +1,72 @@
+const STORE = [
+  {
+      question: "1. Who scores points by lapping members of the opposing team?",
+      answers: [
+      'Jammo',
+      'Jammer',
+      'Boss',
+      'Madam'
+      ],
+      correctAnswer: 'Jammer',
+  },
+  {
+      question: "2. What shape is on the jammer’s helmet?",
+      answers: [
+          'square',
+          'star',
+          'lightning bolt',
+          'heart'
+      ],
+      correctAnswer: 'star',
+  },
+  {
+      question: "3. Which of the following is a legal body part to hit another player with?",
+      answers: [
+          'ELbow',
+          'Knee',
+          'Foot',
+          'Hip'
+      ],
+      correctAnswer: 'Hip',
+  },
+  {
+      question: "4. How many players from the same team can be on the track at the same time?",
+      answers: [
+          '1',
+          '10',
+          '5',
+          '6'
+      ],
+      correctAnswer: '5',
+  },
+  {
+      question: "5. What is a real derby move?",
+      answers: [
+          'The slap your mama',
+          'The apex jump',
+          'The shank',
+          'The ballerina ball jerk'
+        ],
+        correctAnswer: 'The apex jump',
+    },
+    {
+      question: "6. What or who is a pivot?",
+      answers: [
+          'When a skater turns around *super* fast',
+          'The person who starts on the opposing team but switches sides halfway through',
+          'A foul',
+          'A blocker who can potentially become a jammer during a jam'
+      ],
+        correctAnswer: 'A blocker who can potentially become a jammer during a jam',
+    },
+    {
+      question: "7. Who can play derby?",
+      answers: [
+          'Men',
+          'Women',
+          'Women with dark secrets',
+          'Everyone!'
+      ],
+        correctAnswer: 'Everyone!',
+    },
+  ]
